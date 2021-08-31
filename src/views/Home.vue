b7daebc9eb0f4dfd382b3f5130f754621ae7cab6
Baarosh <norbert.lipinski96@gmail.com>
2021-08-31 20:49:59 +0200

    Finished Country and Filters responsively v2.
    Finished Country and Filters responsively v2.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="flex flex-nowrap flex-col items-center md:flex-row md:justify-evenly">
+  <section class="w-auto flex flex-nowrap flex-col items-center md:w-10/12 lg:w-8/12 md:flex-row md:justify-between lg:mt-5 md:ml-26 lg:ml-32 xl:ml-40">
     <TheCounter />
     <TheFilters />
   </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,24 +2,19 @@ module.exports = {
   purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    rotate: {
-      '-180': '-180deg',
-      '-90': '-90deg',
-      '-45': '-45deg',
-      '-20': '-20deg',
-      0: '0',
-      20: '20deg',
-      45: '45deg',
-      90: '90deg',
-      135: '135deg',
-      180: '180deg',
-      270: '270deg',
-    },
     backgroundColor: (theme) => ({
       ...theme('colors'),
       'my-blue': '#0d3558',
     }),
-    extend: {},
+    extend: {
+      margin: {
+        26: '6.5rem',
+      },
+      rotate: {
+        '-20': '-20deg',
+        20: '20deg',
+      },
+    },
   },
   variants: {
     extend: {},
